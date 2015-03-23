166d10f04983d2f6174cd99008a7df400dde06bc
Janusz Slota <janusz.slota@allsaints.com>
2015-03-23 12:03:39 +0000

    WIP

--- src/ganja/plugin/profiler/listener/ProfilerListener.groovy
+++ src/ganja/plugin/profiler/listener/ProfilerListener.groovy
@@ -5,10 +5,12 @@ import ganja.plugin.profiler.Profiler
 
 class ProfilerListener {
 
+    def logger
     Profiler profiler
 
     void onResponse(EngineResponseEvent event) {
 
+        logger?.info("Received event ${event.name}")
         profiler.collect(event.request, event.response, event.exception)
     }
 }
