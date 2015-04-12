80df36e949d9c6744c2bc4451997e538c8e9858d
Janusz Slota <janusz.slota@nixilla.com>
2015-04-12 19:09:53 +0100

    New plugin config

--- src/ganja/plugin/ProfilerPlugin.groovy
+++ src/ganja/plugin/ProfilerPlugin.groovy
@@ -2,38 +2,25 @@ package ganja.plugin
 
 import ganja.common.di.ContainerInterface
 import ganja.common.plugin.PluginInterface
-import ganja.component.di.Reference
-import ganja.component.event.DispatcherInterface
-import ganja.component.http.event.EngineEvents
-import ganja.plugin.profiler.listener.ProfilerListener
+import ganja.component.di.loader.FileLoaderInterface
+import ganja.component.di.loader.YmlFileLoader
+import ganja.component.di.parser.DefinitionParser
+import org.yaml.snakeyaml.Yaml
 
 class ProfilerPlugin implements PluginInterface {
 
+    FileLoaderInterface fileLoader
+    DefinitionParser parser
+
     void registerServices(ContainerInterface container) {
 
-        container
-            .register('profiler.listener', 'ganja.plugin.profiler.listener.ProfilerListener')
-            .setArguments([profiler: new Reference('profiler')])
+        fileLoader = new YmlFileLoader(
+            yaml: new Yaml(),
+            prefix: getClass().getSimpleName()
+        )
 
-        container
-            .register('profiler', 'ganja.plugin.profiler.Profiler')
-            .setArguments([
-                storage: new Reference('profiler.storage')
-            ])
+        parser = new DefinitionParser()
 
-        container
-            .register('profiler.storage', 'ganja.plugin.profiler.storage.ListStorage')
-
-        container
-            .register('dummy.collector', 'ganja.plugin.profiler.collector.DummyCollector')
-    }
-
-    void registerListeners(ContainerInterface container) {
-
-        DispatcherInterface dispatcher = container.get('dispatcher')
-
-        ProfilerListener profilerListener = container.get('profiler.listener')
-        profilerListener.getProfiler().getCollectors().add(container.get('dummy.collector'))
-        dispatcher.addListener(EngineEvents.RESPONSE, profilerListener.&onResponse)
+        parser.parse(fileLoader.load('/config/services.yml') as Map, container)
     }
 }
--- test/ganja/plugin/ProfilerPluginSpec.groovy
+++ test/ganja/plugin/ProfilerPluginSpec.groovy
@@ -1,54 +1,23 @@
 package ganja.plugin
 
-import ganja.common.di.ContainerInterface
-import ganja.component.di.Container
-import ganja.component.di.Definition
-import ganja.component.event.Dispatcher
-import ganja.plugin.profiler.Profiler
-import ganja.plugin.profiler.collector.DataCollectorInterface
-import ganja.plugin.profiler.listener.ProfilerListener
 import spock.lang.Specification
 
 class ProfilerPluginSpec extends Specification {
 
-    void "it can register services in the container"() {
-
-        given:
-        ContainerInterface container = GroovyMock()
-        Definition definition = GroovyMock()
-        def subject = new ProfilerPlugin()
-
-        and:
-        4 * container.register(_,_) >> definition
-
-        when:
-        subject.registerServices(container)
-
-        then:
-        2 * definition.setArguments(_)
-    }
-
-    void "it can register listeners to dispatcher"() {
-
-        given:
-        ContainerInterface container = GroovyMock()
-        Dispatcher dispatcher = GroovyMock()
-        ProfilerListener listener = GroovyMock()
-        Profiler profiler = GroovyMock()
-        DataCollectorInterface collector = GroovyMock()
-        def subject = new ProfilerPlugin()
-
-        and:
-        1 * container.get('dispatcher') >> dispatcher
-        1 * container.get('profiler.listener') >> listener
-        1 * listener.getProfiler() >> profiler
-        1 * profiler.getCollectors() >> []
-        1 * container.get('dummy.collector') >> collector
-
-        when:
-        subject.registerListeners(container)
-
-        then:
-        1 * dispatcher.addListener(_,_)
-    }
+//    void "it can register services in the container"() {
+//
+//        given:
+//        ContainerInterface container = GroovyMock()
+//        Definition definition = GroovyMock()
+//        def subject = new ProfilerPlugin()
+//
+//        and:
+//        4 * container.register(_,_) >> definition
+//
+//        when:
+//        subject.registerServices(container)
+//
+//        then:
+//        2 * definition.setArguments(_)
+//    }
 }
